245ea7336782779d18f74e4ea076accf551d4049
Alcantn1 <nalcantara92@gmail.com>
2023-06-09 21:14:07 -0700

    moved files

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3001,
   user: 'root',
   password: 'Neeknasty69!',
   database: 'employee_tracker_db'
 });
 
-module.exports = connection();
+module.exports = connection;
